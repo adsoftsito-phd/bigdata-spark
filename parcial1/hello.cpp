962ed10e504bf40e7a743b2a73c6240f5d8c8972
adsoftsito-phd <120610355+adsoftsito-phd@users.noreply.github.com>
2023-02-14 21:26:09 +0000

    add hello.cpp

---
+++ parcial1/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // prints the string enclosed in double quotes
+    cout << "hello world" << endl;
+    cout << "hello galaxy" << endl;
+    cout << "hello universe" << endl;
+
+    return 0;
+}
